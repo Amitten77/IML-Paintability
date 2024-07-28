9fd84be0fb32b72107acba7a52ba8d3ed8c1b09a
cty012 <tc37@illinois.edu>
2024-07-29 00:49:51 +0800

    Implement step

--- c/src/game_state.cpp
+++ c/src/game_state.cpp
@@ -54,13 +54,57 @@ bool GameState::apply(RemoverMove move) {
 }
 
 std::vector<GameState> GameState::step() const {
-    // todo
-    return {};
+    std::vector<GameState> result;
+    switch (this->currentPlayer_) {
+        case Player::PUSHER: {
+            std::vector<PusherMove> moves = this->board_.getPusherMoves();
+            std::transform(moves.begin(), moves.end(), result.begin(), [this](const PusherMove& move) {
+                GameState gameState = *this;
+                gameState.apply(move);
+                return gameState;
+            });
+            break;
+        }
+        case Player::REMOVER: {
+            std::vector<RemoverMove> moves = this->board_.getRemoverMoves();
+            std::transform(moves.begin(), moves.end(), result.begin(), [this](const RemoverMove& move) {
+                GameState gameState = *this;
+                gameState.apply(move);
+                return gameState;
+            });
+            break;
+        }
+        case Player::NONE:
+            break;
+    }
+    return result;
 }
 
 std::vector<GameState> GameState::stepPruned() const {
-    // todo
-    return {};
+    std::vector<GameState> result;
+    switch (this->currentPlayer_) {
+        case Player::PUSHER: {
+            std::vector<PusherMove> moves = this->getPusherMovesPruned();
+            std::transform(moves.begin(), moves.end(), result.begin(), [this](const PusherMove& move) {
+                GameState gameState = *this;
+                gameState.apply(move);
+                return gameState;
+            });
+            break;
+        }
+        case Player::REMOVER: {
+            std::vector<RemoverMove> moves = this->getRemoverMovesPruned();
+            std::transform(moves.begin(), moves.end(), result.begin(), [this](const RemoverMove& move) {
+                GameState gameState = *this;
+                gameState.apply(move);
+                return gameState;
+            });
+            break;
+        }
+        case Player::NONE:
+            break;
+    }
+    return result;
 }
 
 const Board& GameState::getBoard() const noexcept {
